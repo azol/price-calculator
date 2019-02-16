3e2f6567e56427b685a5e85e3e65016052cba61b
Artem Zolochevskiy <artem.zolochevskiy@hetzner.de>
2019-02-16 15:35:50 +0200

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,59 @@
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-var del = require('del');
-var htmlmin = require('gulp-htmlmin');
-var sass = require('gulp-sass');
-var inlinesource = require('gulp-inline-source');
-var jshint = require('gulp-jshint');
-var browserSync = require('browser-sync').create();
+const { src, dest, watch, series } = require('gulp');
+const del = require('delete');
+const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass');
+const inlinesource = require('gulp-inline-source');
+const jshint = require('gulp-jshint');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
-var config = {
+const config = {
   app: './',
   tmp: '.tmp/',
   dist: 'dist/'
 };
 
-gulp.task('build', function(callback) {
-  runSequence('build-clean',
-    'lint',
-    'build-css',
-    'build-html',
-    callback);
-});
-
-gulp.task('build-clean', function() {
+function clean() {
   return del([config.tmp, config.dist]);
-});
+}
 
-gulp.task('lint', function() {
-  gulp.src(config.app + 'main.js')
+function lint() {
+  return src(config.app + 'main.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
-});
+}
 
-gulp.task('build-css', function() {
-  return gulp.src(config.app + 'scss/app.scss')
+function buildCss() {
+  return src(config.app + 'scss/app.scss')
     .pipe(sass({outputStyle: 'compressed', includePaths: ['node_modules/foundation-sites/scss']}))
-    .pipe(gulp.dest(config.tmp + 'css'))
-    .pipe(browserSync.stream());
-});
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(dest(config.tmp + 'css'))
+    .pipe(reload({stream: true}));
+}
 
-gulp.task('build-html', function() {
-  gulp.src(config.app + 'price_calculator.html')
+function buildHtml() {
+  return src(config.app + 'price_calculator.html')
     .pipe(inlinesource())
     .pipe(htmlmin({removeComments: true, collapseWhitespace: true}))
-    .pipe(gulp.dest(config.dist));
-});
+    .pipe(dest(config.dist));
+}
 
-gulp.task('default', ['build']);
-
-gulp.task('serve', ['build-css'], function() {
+function serve() {
   browserSync.init({
     server: {
       baseDir: './',
       index: 'price_calculator.html'
     }
   });
-  gulp.watch(config.app + 'scss/*.scss', ['build-css']);
-  gulp.watch(config.app + 'price_calculator.html').on('change', browserSync.reload);
-  gulp.watch(config.app + 'main.js').on('change', browserSync.reload);
-});
+  watch(config.app + 'scss/*.scss', buildCss);
+  watch(config.app + 'price_calculator.html').on('change', reload);
+  watch(config.app + 'main.js').on('change', reload);
+}
+
+exports.clean = clean;
+exports.lint = lint;
+exports.buildCss = buildCss;
+exports.buildHtml = buildHtml;
+exports.serve = serve;
+exports.build = series(clean, lint, buildCss, buildHtml);
+exports.default = exports.build
