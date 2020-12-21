3056dee52b638d964102b1e3fc1221e7cb97cf75
Alexandr Boltris <alexandr.boltris@ru.hetzner.com>
2020-12-21 11:34:27 +0200

    remove PX92

--- main.js
+++ main.js
@@ -308,22 +308,6 @@ PX62-NVMe: 0x SATA HDD + up to 2x NVMe SSDs, up to 6x SATA SSDs, but maximum 8 d
 PX62-NVMe: 1x SATA HDD + 1x NVMe SSD or 1x SATA SSD\n\
 PX62-NVMe: 2x SATA HDDs'
     },
-    PX92: {
-        name: {
-            English: 'Server PX92',
-            Deutsch: 'Server PX92',
-            Русский: 'Сервер PX92'
-        },
-        location: {
-            Germany: {monthly: 99, setup: 99},
-            Finland: {monthly: 94, setup: 99}
-    },
-        comment: 'PX92: Intel® Xeon® W-2145 Octa-Core Skylake W/128 GB DDR4 ECC\n\
-PX92: SSD options: 8x NVMe and SSD (up to 4x NVMe or 6x SSD)\n\
-PX92: HDD options: 1x HDD + 3x NVMe or SSD / 2x HDD + 2x NVMe or SSD / 3x HDD\n\
-PX92: 480 GB NVMe SSD 3D XPoint -- 1x max.\n\
-PX92: max 256 GB RAM DDR4 (4x 32GB)'
-    },
     PX93: {
         name: {
             English: 'Server PX93',
