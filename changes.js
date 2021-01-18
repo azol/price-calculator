c5ca9679753f78ac5af2dd235134699eb64b05d8
Alexandr Boltris <alexandr.boltris@ru.hetzner.com>
2021-01-18 12:47:16 +0200

    updated changelog

--- changes.js
+++ changes.js
@@ -7,13 +7,9 @@ added VAT rates for Australia and South Africa
 sorted countly list
 added 16 TB SATA Enterprise Hard Drive
 updated price for some SATA HDDs
-added 16-Port Hardware RAID-controller (only for SX293)
+added 16-Port Hardware RAID-controller (only for SX133 and SX293)
+updated comments for SX133 and SX293
 
 2021-Jan-14
 added version information and recent changes
-
-2020-Dec-29
-updated price for cPanel/Plesk
-updated RDP license price
-updated VAT rate for Germany
 `;
