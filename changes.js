82a689fd22338186d5eb27cc60b85ef0358293cd
Alexandr Boltris <alexandr.boltris@ru.hetzner.com>
2021-01-18 10:12:06 +0200

    added changelog for 2021 January 18

--- changes.js
+++ changes.js
@@ -1,6 +1,13 @@
-releaseDate = '2021 January 14';
+releaseDate = '2021 January 18';
 
 recentChanges = `Recent changes:
+
+2021-Jan-18
+added VAT rates for Australia and South Africa
+sorted countly list
+added 16 TB SATA Enterprise Hard Drive
+updated price for some SATA HDDs
+added 16-Port Hardware RAID-controller (only for SX293)
 
 2021-Jan-14
 added version information and recent changes
