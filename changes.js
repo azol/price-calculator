1ac8fa670ba2e86605298b4f07b24ed1ddd34ae9
Pavel Mishin <pavel.mishin@ru.hetzner.com>
2021-03-23 16:24:33 +0200

    updated changelog

--- changes.js
+++ changes.js
@@ -1,18 +1,10 @@
-releaseDate = '2021 February 02';
+releaseDate = '2021 March 23';
 
 recentChanges = `Recent changes:
 
-2021-Feb-02
-removed outdated models:
-SX62, SX132, SX292
-SX61, SX131, SX291
-DX141, DX151, DX180, DX291
+2021-Mar-23
+added 2 TB NVMe SSD
 
-2021-Jan-18
-added VAT rates for Australia and South Africa
-sorted countly list
-added 16 TB SATA Enterprise Hard Drive
-updated price for some SATA HDDs
-added 16-Port Hardware RAID-controller (only for SX133 and SX293)
-updated comments for SX133 and SX293
+2021-Mar-23
+changed disks and big LAN switches prices
 `;
