28ee3da82228de5a46ba12527a5a98a5db2c7734
Alexandr Boltris <alexandr.boltris@ru.hetzner.com>
2021-04-21 10:35:02 +0200

    update changelog

--- changes.js
+++ changes.js
@@ -1,6 +1,9 @@
-releaseDate = '2021 March 23';
+releaseDate = '2021 April 21';
 
 recentChanges = `Recent changes:
+
+2021-Apr-21
+update comments about RAM limits for DX181 and PX93
 
 2021-Mar-23
 added 2 TB NVMe SSD
