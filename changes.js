c4a795750da92c51398618138d81487d99713ffb
Alexandr Boltris <alexandr.boltris@ru.hetzner.com>
2021-05-21 12:22:50 +0200

    updated changelog

--- changes.js
+++ changes.js
@@ -2,6 +2,10 @@ releaseDate = '2021 May 19';
 
 recentChanges = `Recent changes:
 
+2021-May-21
+Fix typo in DX server name (DX181 was named as DX180)
+Rename DX181 in order to have similar name to other DX servers
+
 2021-May-19
 Enable Dell-line in Finland location
 `;
