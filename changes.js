0fbb6c70f06fa611c1f9f2d6bcdaea3cc2f65760
Alexandr Boltris <alexandr.boltris@ru.hetzner.com>
2021-06-28 17:03:18 +0200

    update changes.js

--- changes.js
+++ changes.js
@@ -1,6 +1,10 @@
-releaseDate = '2021 Jun 17';
+releaseDate = '2021 Jun 29';
 
 recentChanges = `Recent changes:
+
+2021-Jun-29
+Remove SX63, SX133, and SX293 servers
+Add SX64, SX134, and SX294 servers
 
 2021-Jun-17
 Change the setup for the AX101 from € 99 to € 199
@@ -8,7 +12,4 @@ Change the setup for the AX101 from € 99 to € 199
 2021-May-21
 Fix typo in DX server name (DX181 was named as DX180)
 Rename DX181 in order to have similar name to other DX servers
-
-2021-May-19
-Enable Dell-line in Finland location
 `;
