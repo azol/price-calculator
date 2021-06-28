184e0be28b3cae754311ae46fca3100487c5a281
Alexandr Boltris <alexandr.boltris@ru.hetzner.com>
2021-06-28 17:15:17 +0200

    update changes.js

--- changes.js
+++ changes.js
@@ -5,6 +5,7 @@ recentChanges = `Recent changes:
 2021-Jun-29
 Remove SX63, SX133, and SX293 servers
 Add SX64, SX134, and SX294 servers
+Fix release date output format
 
 2021-Jun-17
 Change the setup for the AX101 from € 99 to € 199
