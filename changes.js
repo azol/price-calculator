b9185b8d5fc06e869f5c00b800fcd55a86333789
Alexandr Boltris <alexandr.boltris@ru.hetzner.com>
2021-07-12 10:07:23 +0200

    update changes.js

--- changes.js
+++ changes.js
@@ -1,6 +1,9 @@
-releaseDate = '2021 Jun 29';
+releaseDate = '2021 Jul 07';
 
 recentChanges = `Recent changes:
+
+2021-Jul-07
+Fix additional RAM description for PX93
 
 2021-Jun-29
 Remove SX63, SX133, and SX293 servers
@@ -9,8 +12,4 @@ Fix release date output format
 
 2021-Jun-17
 Change the setup for the AX101 from € 99 to € 199
-
-2021-May-21
-Fix typo in DX server name (DX181 was named as DX180)
-Rename DX181 in order to have similar name to other DX servers
 `;
